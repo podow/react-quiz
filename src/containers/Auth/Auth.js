2ec7b32b54423eed46a3d18fdf9c781f3e576ecd
Bogdan <podowko@gmail.com>
2018-11-03 00:45:40 +0600

    add auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,8 @@ import styles from './Auth.module.scss'
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 
+import axios from 'axios'
+
 class Auth extends Component {
     state = {
         isFormValid: false,
@@ -37,12 +39,36 @@ class Auth extends Component {
         }
     };
 
-    loginHandler = () => {
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
 
+        try {
+            const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDyVNDO4XnsgA39NDvWqWLGKY5IzfSlUkQ', authData);
+
+            console.log(response);
+        } catch (e) {
+            console.warn(e);
+        }
     };
 
-    registerHandler = () => {
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
 
+        try {
+            const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDyVNDO4XnsgA39NDvWqWLGKY5IzfSlUkQ', authData);
+
+            console.log(response);
+        } catch (e) {
+            console.warn(e);
+        }
     };
 
     validateControl = (value, validation) => {
